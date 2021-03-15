a587a7f07422e561a1e9c4addff94792f4c647cd
Mark Wlodawski <imakethesites@gmail.com>
2021-03-15 10:30:32 -0400

    Full CRUD functionality added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyApTP2vEAUPqlbfhSfjqPLTkjl_9uH5O8M',
+  authDomain: 'vue-calendarski.firebaseapp.com',
+  projectId: 'vue-calendarski',
+  storageBucket: 'vue-calendarski.appspot.com',
+  messagingSenderId: '76831155984',
+  appId: '1:76831155984:web:03c4c27b6295de6af3a795',
+});
+
+export const db = firebase.firestore();
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
